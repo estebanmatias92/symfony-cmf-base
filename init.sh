82dbf4660b12726770b1a8ddba8ad31c87453184
estebanmatias92 <estebanmatias92@gmail.com>
2015-03-05 15:53:56 -0300

    Clean init script

--- init.sh
+++ init.sh
@@ -1,26 +1,10 @@
 #!/bin/bash
 
-volume=".:/var/www"
-newVolume="./tmp-project:/var/www"
-
-install_dependencies() {
-    mkdir tmp-project
-    sed -i 's|'$volume'|'$newVolume'|' docker-compose.yml
-
-    docker-compose up -d --no-deps data
-    docker-compose run --rm composer require --dev --no-interaction --prefer-dist "phpunit/phpunit:~4.4" "codeception/codeception:~2.0"
-    docker-compose stop
-    docker-compose rm --force
-
-    sed -i 's|'$newVolume'|'$volume'|' docker-compose.yml
-    cp -Rf tmp-project/* ./
-    sudo rm -rf tmp-project
-}
-
-install_dependencies
-
 # Init services
 docker-compose up -d data dbData db dbAmbassador php phpAmbassador server
+
+# Install project
+docker-compose run --rm composer require --dev --no-interaction --prefer-dist "phpunit/phpunit:~4.4" "codeception/codeception:~2.0"
 
 # Set up database
 #docker-compose run --rm console doctrine:database:create
