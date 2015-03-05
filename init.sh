38db39123fc47051c8a63f8fc2e2e5bb4dd23cf3
estebanmatias92 <estebanmatias92@gmail.com>
2015-03-05 00:28:00 -0300

    Create temporary folder to install composer project

--- init.sh
+++ init.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
+volume=".:/var/www"
+newVolume="./tmp-project:/var/www"
+
+install_dependencies() {
+    mkdir tmp-project
+    sed -i 's|'$volume'|'$newVolume'|' docker-compose.yml
+
+    docker-compose up -d --no-deps data
+    docker-compose run --rm composer require --dev --no-interaction --prefer-dist "phpunit/phpunit:~4.4" "codeception/codeception:~2.0"
+    docker-compose stop
+    docker-compose rm --force
+
+    sed -i 's|'$newVolume'|'$volume'|' docker-compose.yml
+    cp -Rf tmp-project/* ./
+    sudo rm -rf tmp-project
+}
+
+install_dependencies
+
 # Init services
 docker-compose up -d data dbData db dbAmbassador php phpAmbassador server
 
-# Delay to give to the db container install everything
-sleep 60s
-
-# Install project dependencies
-docker-compose run --rm composer require --dev --no-update "phpunit/phpunit:~4.4" "codeception/codeception:~2.0"
-docker-compose run --rm composer update --no-interaction --prefer-dist
-
 # Set up database
 #docker-compose run --rm console doctrine:database:create
-docker-compose run --rm console doctrine:phpcr:init:dbal
+sleep 60 && docker-compose run --rm console doctrine:phpcr:init:dbal
 docker-compose run --rm console doctrine:phpcr:repository:init
-docker-compose run --rm console doctrine:phpcr:fixtures:load
+docker-compose run --rm console doctrine:phpcr:f:ixtures:load
